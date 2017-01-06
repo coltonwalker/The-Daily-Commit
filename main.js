b380e3c89ccae0c8e9c47b94bcc8e9a22f7357b0
Cole Walker <coltonwalker@amdg.rockhursths.edu>
2017-01-06 11:26:27 -0600

    SecondDayPush

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
 // this statement is a redirect for brackets development.
-if(window.location.hostnames === '127.0.0.1') {
+if(window.location.hostname === '127.0.0.1') {
     window.location = 'http://localhost:1898';
 }
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
+}
+
+$('.g-signin2').hide();
+
+ function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
+
+$('')
