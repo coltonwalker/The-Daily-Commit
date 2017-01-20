66bd0f023aa3ac5c720d8fa9274e79d337fefb48
Cole Walker <coltonwalker@amdg.rockhursths.edu>
2017-01-20 10:49:52 -0600

    SeventhDayPush

---
+++ node/server.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var Mongo = require('mongodb').MongoClient;
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var mongo_url = 'mongodb://localhost:27017/apcsp';
+
+//static MongoDB operations
+Mongo.connect(mongo_url, function (err, db) {
+    
+    if (err) {
+        console.log('MongoDB connection error');
+    } else {
+        console.log('Connectedd to MongoDB');
+    }
+});
